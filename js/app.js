18f77a3cace5bec285c4e2a74f61d3bfe8881983
robeerob <rob@rob.ee>
2016-04-21 11:10:09 +0200

    Add json list, top headline and description text, remove textarea.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,41 @@
+var commandEntries = [
+  {
+    "title" : "grep",
+    "syntax" : "grep [OPTIONS] PATTERN [FILE...]",
+    "example" : "grep milk shoppinglist.txt",
+    "description" : "grep searches the named input FILEs for lines containing a match to the given PATTERN."
+  },
+  {
+    "title": "grep",
+    "syntax" : "ls [OPTION]... [FILE]...",
+    "example": "ls -lah",
+    "description": "List information about the FILEs (the current directory by default)."
+  },
+  {
+    "title": "cut",
+    "syntax" : "cut [OPTION]... [FILE]...",
+    "example": "cut -c2 test.txt",
+    "description": "Print selected parts of lines from each FILE to standard output."
+  },
+  {
+    "title": "cat",
+    "syntax" : "cat [OPTION]... [FILE]...",
+    "example": "cat shoppinglist.txt",
+    "description": "Concatenate FILE(s) to standard output."
+  },
+  {
+    "title": "wc",
+    "syntax" : "wc [OPTION]... [FILE]...",
+    "example": "wc shoppinglist.txt",
+    "description": " Print newline, word, and byte counts for each FILE, and a total line if more than one FILE is specified."
+  }
+];
+
 $(document).ready(function() {
-  $("#topbtn").click(function() {
+  $(".cmdresult").append({
+    
+  });
+  $("#topbutton").click(function() {
     var searchEntered = $("#topsearch").val();
     console.log("click test");
     $(".cmdresult").hide();
