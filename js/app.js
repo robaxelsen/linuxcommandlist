2e86b2fd19e0f0df80f58cb3f830f1bb5cdff5c0
robeerob <rob@rob.ee>
2016-05-03 14:15:32 +0200

    Add comments

--- js/app.js
+++ js/app.js
@@ -1,12 +1,11 @@
 
-//TODO: Add more comments, and keep focus on good comments
-//TODO: Remove submit button when enterKey function implemented
 //TODO: Add about and contact page in dropdown-menu titled Menu
 //TODO: Create logo
 //TODO: Decide on whether or not to keep more info button/icons
 //TODO: Decide on whther or not to use bootflat
 //TODO: #topsearch is not styled good for mobile, and does not work well as a dropdown element. Put it under navbar instead.
 
+// JSON object for the linux command entries
 var commandEntries = [
   {
     "title" : "grep",
@@ -40,22 +39,27 @@ var commandEntries = [
   }
 ];
 
+// jQuery function to make jQuery fire when document is ready
 $(document).ready(function() {
 
+  // Lists all items in commandEntries object, in given html format
+  commandEntries.forEach( function(item) {
+    $(".toprow").append('<div class="col-md-3 cmdresult"><div class="thumbnail"><div class="caption"><h3 class="">' + item.title + '</h4><p><strong>' + item.syntax + '</strong></p><p><strong>Example:</strong> ' + item.example + '</p><p>' + item.description + '</p> <a href="#" class="btn btn-default btn-xs pull-right" role="button"><i class="glyphicon glyphicon-edit"></i></a>  <a href="#" class="btn btn-default btn-xs" role="button">More Info</a></div></div></div>');
+  });
 
+  // Function to fire when keypress Enter is done in input field
   $("input").on("keydown",function search(e) {
     if(e.keyCode == 13) {
+      // Prevent default form submit behavior
       event.preventDefault();
+      // Hides all current displayed results, then shows only the ones with title matching search value
       var searchEntered = $("#topsearch").val();
       $(".cmdresult").hide();
       $(".cmdresult:contains('" + searchEntered + "')").show();
     }
   });
 
-  commandEntries.forEach( function(item) {
-    $(".toprow").append('<div class="col-md-3 cmdresult"><div class="thumbnail"><div class="caption"><h3 class="">' + item.title + '</h4><p><strong>' + item.syntax + '</strong></p><p><strong>Example:</strong> ' + item.example + '</p><p>' + item.description + '</p> <a href="#" class="btn btn-default btn-xs pull-right" role="button"><i class="glyphicon glyphicon-edit"></i></a>  <a href="#" class="btn btn-default btn-xs" role="button">More Info</a></div></div></div>');
-  });
-
+  // http://brm.io/jquery-match-height/ jQuery plugin to make sure .cmdresult divs have equal heights
   $(function() {
     // apply your matchHeight on DOM ready (they will be automatically re-applied on load or resize)
     // get test settings
@@ -67,4 +71,5 @@ $(document).ready(function() {
       });
     });
   });
+
 });
